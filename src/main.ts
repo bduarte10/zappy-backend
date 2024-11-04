15194e1ed6744548e256984f2f6dd9f2e060e306
Bruno Duarte <duartebruno2208@gmail.com>
2024-11-04 00:21:20 -0300

    altera a porta padrão do servidor para 3001

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(port, '0.0.0.0');
