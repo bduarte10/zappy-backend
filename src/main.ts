7629d41232260f1b47d22b244811f403394b683a
Bruno Duarte <duartebruno2208@gmail.com>
2024-11-03 23:03:36 -0300

    altera a configuração da porta do servidor para usar a variável de ambiente PORT

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
