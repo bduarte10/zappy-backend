7fbac2e148d6be1e0f068ca966262a22fbaa2e47
Bruno Duarte <duartebruno2208@gmail.com>
2024-11-11 08:47:37 -0300

    altera a porta padrão do servidor de 3001 para 8080

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 8080;
   const host = process.env.HOSTNAME || '0.0.0.0';
   const app = await NestFactory.create(AppModule);
   app.enableCors();
