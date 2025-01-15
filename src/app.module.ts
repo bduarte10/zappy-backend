caf38b721e11e27e5b243521e06c9f8d398d0b7f
Bruno Duarte <duartebruno2208@gmail.com>
2025-01-14 21:52:34 -0300

    remove AppController and AppService; implement PrismaService for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { WhatsappModule } from './whatsapp/whatsapp.module';
 
 @Module({
   imports: [WhatsappModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
